abf55d92a4b172c48186e2aa2e8f85678a7d2bb5
Cody-Junier <90635812+Cody-Junier@users.noreply.github.com>
2021-11-08 13:05:36 -0600

    learning luxon

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var DateTime = luxon.DateTime;
+var dt= DateTime.local().toLocaleString(DateTime.DATETIME_FULL);
+var currentDayEl= $("#currentDay").text(dt);
+currentDayEl
+
+// console.log(dt.valueOf(hour))
+var checkTime = function(taskEl){
+    if (currentDayEl);
+  };
+  
+
+
+// var today= DateTime.now();
+// var fullTime= DateTime.toLocaleString(DateTime.DATETIME_FULL)
+console.log(DateTime);
+console.log(DateTime.local().plus({ hours: 3, minutes: 13 }));
+console.log(DateTime.local().toLocaleString(DateTime.DATETIME_FULL))
+
