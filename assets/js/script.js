f8b3f21d7c406c9979cafe51524215fec40932cf
Cody-Junier <90635812+Cody-Junier@users.noreply.github.com>
2021-11-10 18:05:03 -0600

    Got rid of Luxon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
-var DateTime = luxon.DateTime;
-var dt= DateTime.local().toLocaleString(DateTime.DATETIME_FULL);
-var currentDayEl= $("#currentDay").text(dt);
-currentDayEl
+// Make + populate time at top of page
+var DateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-// console.log(dt.valueOf(hour))
-var checkTime = function(taskEl){
-    if (currentDayEl);
-  };
-  
+var currentDayEl= $("#currentDay");
+currentDayEl.text(DateTime)
 
 
-// var today= DateTime.now();
-// var fullTime= DateTime.toLocaleString(DateTime.DATETIME_FULL)
-console.log(DateTime);
-console.log(DateTime.local().plus({ hours: 3, minutes: 13 }));
-console.log(DateTime.local().toLocaleString(DateTime.DATETIME_FULL))
+
+
+
+
+// Save schedule in local storage
+// make elements 
+const inputEl = $('.description').text();
+const saveEl = $(".saveBtn");
+// whenever save is clicked, grab input and save to local storage
+saveEl.click(function(){
+    console.log("save-clicked");
+    localStorage.setItem("description",inputEl);
+})
+// load local storage on refresh
+var loadTasks = function() {
+   localStorage.getItem(inputEl);
+};
+loadTasks();
+
 
