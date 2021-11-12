59e4be132762c160f8b5ece1bcaff64121827f44
Cody-Junier <90635812+Cody-Junier@users.noreply.github.com>
2021-11-12 14:29:06 -0600

    Removed Jquery in js file, completed MVP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,56 @@
 // Make + populate time at top of page
 var DateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-var currentDayEl= $("#currentDay");
-currentDayEl.text(DateTime)
+var currentDayEl= document.querySelector("#currentDay");
+currentDayEl.textContent=DateTime;
+
+var saveBtnEl= document.querySelectorAll(".saveBtn")
+
+var textContentEl= document.querySelectorAll(".description");
 
 
-
-
-
-
-// Save schedule in local storage
-// make elements 
-const inputEl = $('.description').text();
-const saveEl = $(".saveBtn");
-// let textAreas =["$('#text-hour-9')","$('#text-hour-10')","$('#text-hour-11')","$('#text-hour-12')","$('#text-hour-13')","$('#text-hour-14')","$('#text-hour-15')","$('#text-hour-16')","$('#text-hour-17')"]
-// console.log(textAreas[1])
-// textAreas[1].text()=""
-// whenever save is clicked, grab input and save to local storage
-
-for (let index = 9; index < 17; index++) {
-
-    saveEl.on('click', function(){
-        console.log("save-clicked");
-        let text= $('#text-hour-'+index).val();
-        localStorage.setItem("description",text);
+// Save input for each textarea
+var saveContent= function(){
+    for (let index = 0; index < textContentEl.length; index++) {
+        const text = textContentEl[index];
+        console.log(text.value);
+        localStorage.setItem("task"+index,text.value);
         
-    });
+    }
 }
-// load local storage on refresh
-var loadTasks = function() {
+// Put above saved input into local storage
+for (let index = 0; index < saveBtnEl.length; index++) {
+    const element = saveBtnEl[index];
+    element.addEventListener("click", saveContent);
     
-
-   localStorage.getItem("description");
+}
+// load local storage on refresh, and populate in textarea
+var loadTasks = function() {
+    for (let index = 0; index < textContentEl.length; index++) {
+        const text = localStorage.getItem("task"+index);
+        textContentEl[index].value=text;
+    }
 };
+// check on current time vs task time, change color of background based on time
+var colorCheck = function(){
+    var currentTime= moment().hours();
+    for (let index = 0; index < textContentEl.length; index++) {
+        const color = textContentEl[index];
+        if (currentTime < color.id){
+            color.setAttribute("class", "bg-success description col-10")
+            
+        }
+        else if (currentTime > color.id){
+            color.setAttribute("class", "bg-secondary description col-10")
+            
+        }
+        else if (currentTime = color.id){
+            color.setAttribute("class", "bg-danger description col-10")
+            
+        }
+
+    }
+}
+
 loadTasks();
-
-
+colorCheck();
